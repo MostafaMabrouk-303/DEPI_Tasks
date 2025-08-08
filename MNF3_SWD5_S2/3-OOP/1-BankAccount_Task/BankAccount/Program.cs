d1df246b1601bc6f5d43e7f8bd20543172d437c5
Mostafa Mabrouk <mostafamabrouk4466@gmail.com>
2025-08-08 18:41:52 +0300

    Add files via upload

---
+++ MNF3_SWD5_S2/3-OOP/1-BankAccount_Task/BankAccount/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace BankAccount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            //Create  BankAccount objects using different constructors
+            
+            BankAccount B1= new BankAccount();
+            BankAccount B2 = new BankAccount("MostafaMabrouk","12345678993456","01286674711","Cairo",3000.11);
+            BankAccount B3= new BankAccount("Mohamed Ali", "12345678993455", "01066674711");
+
+
+            // Call ShowAccountDetails() for each to display their info.
+            B1.ShowAccountDetails(B1);
+            B2.ShowAccountDetails(B2);
+            B3.ShowAccountDetails(B3);
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
