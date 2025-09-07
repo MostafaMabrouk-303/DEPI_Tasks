58f8220eba74eb7199ff742384f932fdc7e96285
Mostafa Mabrouk <mostafamabrouk4466@gmail.com>
2025-09-07 18:55:24 +0300

    Add files via upload

---
+++ MNF3_SWD5_S2/4-CSharp Advanced/Deip_Sol/Sol/Program.cs
@@ -0,0 +1,92 @@
+﻿namespace Sol
+{
+
+    static class Extentions
+    {
+        public static int CalcExtentionMethod(this int X , int Y, Func<int,int,int> Operation)
+        {
+          
+            return Operation(X, Y);
+
+        }
+
+
+    }
+
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter Number 1 : ");
+            int X=int.Parse(Console.ReadLine());
+
+
+            Console.Write("Enter Number 2 : ");
+            int Y=int.Parse(Console.ReadLine());
+
+            Console.Clear();
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("1. + ");
+            Console.WriteLine("2. - ");
+            Console.WriteLine("3. * ");
+            Console.WriteLine("4. / ");
+            Console.WriteLine("5. % ");
+            Console.WriteLine("-------------------------------------------------");
+
+            Console.Write("Choice Number of OPeration : ");
+            short Op = short.Parse(Console.ReadLine());
+
+            Console.WriteLine("-------------------------------------------------");
+
+            int Result = 0;
+
+            switch (Op)
+            {
+                case 1:
+                    Result = X.CalcExtentionMethod(Y, (X, Y) => X + Y);
+
+                break;
+
+                case 2:
+                    Result = X.CalcExtentionMethod(Y, (X, Y) => X - Y);
+
+                break;
+
+                case 3:
+                    Result = X.CalcExtentionMethod(Y, (X, Y) => X * Y);
+
+                break;
+
+                case 4:
+                    Result = X.CalcExtentionMethod(Y, (X, Y) => X / Y);
+
+                break;
+
+                case 5:
+                    Result = X.CalcExtentionMethod(Y, (X, Y) => X % Y);
+
+                break;
+
+                default:
+                    Console.WriteLine("Choice correct OPeration ");
+                break;
+            }
+
+
+
+
+           
+            Console.WriteLine($"Result = {Result}");
+            Console.WriteLine("-------------------------------------------------");
+
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
