eeb66158a4c4c8379dfbc08ba6f9a74f8d1412c0
Mostafa Mabrouk <mostafamabrouk4466@gmail.com>
2025-09-07 18:59:24 +0300

    Add files via upload

---
+++ MNF3_SWD5_S2/5-Linq/Depi_Linq/SolofLinq_2/Program.cs
@@ -0,0 +1,129 @@
+﻿
+using LINQtoObject;
+using System.Diagnostics;
+using System.Linq;
+using static LINQtoObject.SampleData;
+
+namespace SolofLinq_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ///1-Display book title and its ISBN.           
+            ///foreach (var book in Books)
+            ///    Console.WriteLine($"Book Title: {book.Title} , Book ISBN: {book.Isbn}");
+            ///
+
+            ///2-Display the first 3 books with price more than 25.
+            /// var Result = Books.Where(B => B.Price > 25).Take(3).ToList();
+            /// foreach (var book in Result)
+            ///     Console.WriteLine(book);
+
+            ///3-Display Book title along with its publisher name. (Using 2 methods).
+            ///1.
+            ///var Result = Books.Select
+            ///    (b => new { BookTitle = b.Title, PublisherName = b.Publisher.Name });
+            ///2.
+            ///var Result = from b in Books
+            ///             select new { BookTitle = b.Title, PublisherName = b.Publisher.Name };
+            ///foreach (var b in Result)
+            ///    Console.WriteLine($"\"{b.BookTitle}\" Published By \"{b.PublisherName}\"");
+
+            ///4-Find the number of books which cost more than 20.        
+            /// var Result = Books.Where(B => B.Price > 20).Count();
+            ///Console.WriteLine($"Number Of Books = {Result}");
+
+            ///5-Display book title, price and subject name sorted by its subject name ascending and by its price descending.           
+            ///var Result = (from b in Books
+            ///              orderby b.Subject.Name
+            ///              orderby b.Price descending
+            ///              select new { Title = b.Title, Price = b.Price, Subject = b.Subject });
+            ///foreach (var B in Result)
+            ///    Console.WriteLine($"Title = {B.Title} , Price = {B.Price} , Subject = {B.Subject} .");
+
+            ///6-Display All subjects with books related to this subject. (Using 2 methods).
+            ///1.
+            /// var Result = from b in Books
+            ///              group b by b.Subject;
+            /// 
+            /// foreach (var group in Result)
+            /// {
+            ///     Console.WriteLine(group.Key);
+            /// 
+            ///     foreach (var book in group)
+            ///     {
+            ///         Console.WriteLine($"............{book.Title}");
+            ///     }
+            /// }
+            /// 
+            /// 2.
+            /// var Result = Books.GroupBy(b=>b.Subject).ToList();
+            /// 
+            /// foreach (var group in Result)
+            /// {
+            ///     Console.WriteLine(group.Key);
+            /// 
+            ///     foreach (var book in group)
+            ///     {
+            ///      Console.WriteLine($"............{book.Title}");
+            ///     }
+            /// }
+
+            ///7-Try to display book title & price (from book objects) returned from GetBooks Function.
+            /// var Result = GetBooks();
+            /// foreach (var obj in Result)
+            /// {
+            ///     Book b = (Book)obj;
+            ///     Console.WriteLine($"Book Title : {b.Title} , Book Price = {b.Price}\n");
+            /// }
+
+            ///8-Display books grouped by publisher & Subject.
+            ///  var Result = from b in Books
+            ///               group b by b.Publisher
+            ///              into Group
+            ///               from g in Group
+            ///               group g by g.Subject;
+            /// 
+            /// foreach (var group in Result)
+            /// {
+            ///     Console.WriteLine(group.Key);
+            ///
+            ///         foreach (var item in group)
+            ///         {
+            ///     Console.WriteLine($"{item.Publisher} :: {item.Price}");
+            ///         }
+            /// }
+
+            ///Bonus
+            /// string PublisherName;
+            /// 
+            /// string[] PublishersNAme = Publishers.Select(p => p.Name).ToArray();
+            /// 
+            /// Console.Write("Publishers Names : ");
+            /// foreach (var pup in PublishersNAme)
+            ///     Console.Write($"{pup} ..");
+            /// 
+            /// Console.WriteLine("\n---------------------------------------------------------");
+            /// 
+            /// 
+            /// Console.Write("Enter Publisher Name : ");
+            /// PublisherName = Console.ReadLine();
+            /// 
+            /// 
+            /// if (!PublishersNAme.Contains(PublisherName))
+            /// {
+            ///     Console.WriteLine("this Publisher isn't Exist !!");
+            ///     return ;
+            /// }
+            /// 
+            /// var Result = Books.Where(b => b.Publisher.Name == PublisherName);
+            /// 
+            /// SortingClass.FindBooksSorted(Result);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MNF3_SWD5_S2/5-Linq/Depi_Linq/SolofLinq_2/SortingClass.cs
@@ -0,0 +1,136 @@
+﻿using LINQtoObject;
+using System.Threading.Tasks;
+using static LINQtoObject.SampleData;
+
+namespace SolofLinq_2
+{
+    internal class SortingClass
+    {
+
+        public static void FindBooksSorted(IEnumerable<Book> Result)
+        {
+            short SortingMehtod, DescOrAsc;
+
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("------------------Sortting Way--------------------");
+            Console.WriteLine("1 .Book Title");
+            Console.WriteLine("2 .Book PageCount");
+            Console.WriteLine("3 .Book Price");
+            Console.WriteLine("4 .Book Isbn");
+            Console.WriteLine("5 .Book Subject");
+            Console.WriteLine("---------------------------------------------------------");
+            Console.Write("Enter Number for Way of Sorting : ");
+            SortingMehtod = short.Parse(Console.ReadLine());
+
+            Console.Clear();
+
+            Console.WriteLine("1.ASC");
+            Console.WriteLine("2.DESC");
+
+            Console.WriteLine("---------------------------------------------------------");
+            Console.Write("Enter Number of Sorting way : ");
+            DescOrAsc = short.Parse(Console.ReadLine());
+
+            //var Result = Books;
+
+            switch (SortingMehtod)
+            {
+                //By Title
+                case 1:
+                    if (DescOrAsc == 1)
+                    {
+                        Result = Result.OrderBy(p => p.Title);
+                    }
+                    else
+                    {
+                        Result = Result.OrderByDescending(p => p.Title);
+                    }
+
+
+                    break;
+
+                // By PageCount
+                case 2:
+                    if (DescOrAsc == 1)
+                    {
+                        Result = Result.OrderBy(p => p.PageCount);
+                    }
+                    else
+                    {
+                        Result = Result.OrderByDescending(p => p.PageCount);
+                    }
+
+
+                    break;
+
+
+                //By Price
+                case 3:
+                    if (DescOrAsc == 1)
+                    {
+                        Result = Result.OrderBy(p => p.Price);
+                    }
+                    else
+                    {
+                        Result = Result.OrderByDescending(p => p.Price);
+                    }
+
+
+                    break;
+
+
+                //By Isbn
+                case 4:
+                    if (DescOrAsc == 1)
+                    {
+                        Result = Result.OrderBy(p => p.Isbn);
+                    }
+                    else
+                    {
+                        Result = Result.OrderByDescending(p => p.Isbn);
+                    }
+
+
+                    break;
+
+
+                //By Subject
+                case 5:
+                    if (DescOrAsc == 1)
+                    {
+                        Result = Result.OrderBy(p => p.Subject.Name);
+                    }
+                    else
+                    {
+                        Result = Result.OrderByDescending(p => p.Subject.Name);
+                    }
+
+
+                    break;
+
+
+            }
+
+            Console.Clear();
+            if (Result.Count() == 0)
+            {
+                Console.WriteLine("there is No Exist Books ");
+                return;
+            }
+            Console.WriteLine($"there are {Result.Count()} Books After Sorting : ");
+            Console.WriteLine("---------------------------------------------------------");
+
+            foreach (var item in Result)
+                Console.WriteLine(item.Title);
+            
+
+        }
+
+
+
+
+
+
+
+    }
+}
